623de48983afa391ef8adae4fbfc7938b6885534
Evan Chernicky <echernicky@gmail.com>
2022-02-14 14:04:50 -0500

    add category id attribute to each cell

--- src/index.js
+++ src/index.js
@@ -4,34 +4,38 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(response => response.json())
         .then(data => renderCategories(data))
     }
-    console.log()
-    fetchCategories();
+
+    fetchCategories()
 
     function renderCategories(data) {
+
+
+        //Adds category ID to cells
+        const columnsData = document.getElementsByClassName('question-column')
+        const columns = Array.from(columnsData)
+       
+        for (let i = 0;  i <= 5;  i++){ //loop through 6 times (for each category and row)
+
+            cellsData = columns[i].getElementsByClassName('cell')
+            cells = Array.from(cellsData)
+            cells.forEach(cell => cell.setAttribute('category-id', data[i].id))
+
+        }
+        
+
+        //Adds categories to category row
         data.forEach(categoryData => {
+
             const categoryRow = document.getElementById('categoryRow')
             const cell = document.createElement('div')
             cell.className = 'cell'
             cell.innerText = categoryData.title
             categoryRow.appendChild(cell)
 
-            fetch(`https://jservice.io//api/clues?category=${categoryData.id}`)
-            .then(response => response.json())
-            .then(clueData => renderQuestions(clueData))
-
-            function renderQuestions (clueData) {
-
-                clueData
-
-                for (let clue of clueData) {
-                    console.log(clue)
-                }
-
-                console.log(clueData)
-
-            }
-
-
         })
+
     }
+
 })
+
+

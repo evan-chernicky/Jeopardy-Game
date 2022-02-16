b9e56181133d77a9489873c9bcf40d847504b21a
Evan Chernicky <echernicky@gmail.com>
2022-02-16 16:49:00 -0500

    fixed clue answer bug (but for real this time...)

--- src/index.js
+++ src/index.js
@@ -66,9 +66,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
                 //Getting question and answer
                 const question = clue.question
-                const clueAnswer = clue.answer.replace(/[^\w\s!?]/g,'')
-                clueAnswer.replace('<i>','')
-                clueAnswer.replace('</i>','')
+                const clueAnswer = clue.answer.replace('</i>','').replace('<i>','').replace(/[^\w\s!?]/g,'')
+
 
                 
                 //Add values

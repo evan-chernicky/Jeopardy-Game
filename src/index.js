f8bf29cc06fa79605b18050be917ae3c92468f73
Jane Kim <jane.kim0717@gmail.com>
2022-02-18 10:30:54 -0500

    Cleaned up code; remove extra spaces

--- src/index.js
+++ src/index.js
@@ -12,34 +12,23 @@ document.addEventListener("DOMContentLoaded", function() {
     fetchCategories()
 
     function renderCategories(data) {
-
-
-
         //Adds category ID to cells
         const columns = Array.from(document.getElementsByClassName('question-column'))
        
         for (let i = 0;  i <= 5;  i++){ //loop through 6 times (for each category and row)
-
             cellsData = columns[i].getElementsByClassName('cell')
             let cells = Array.from(cellsData)
             cells.forEach(cell => cell.setAttribute('category-id', data[i].id))
-
-        }
-    
+        } 
         //Adds categories to category row
         data.forEach(categoryData => {
-
             const categoryRow = document.getElementById('categoryRow')
             const cell = document.createElement('div')
             cell.className = 'cell'
             cell.innerText = categoryData.title
             categoryRow.appendChild(cell)
-
         })
-
     }
-
-
     //Fetch data on cell click
     let cellHTML = document.querySelectorAll('.cell');
     let cellQ = Array.from(cellHTML)
@@ -48,7 +37,6 @@ document.addEventListener("DOMContentLoaded", function() {
     scoreSpan.innerText = score
     let turns = 30
     const modalContent = document.querySelector('.modal-content')
-
     
     function startGame () {
         const modalStart = document.querySelector("#questionPopUp");
@@ -64,25 +52,17 @@ document.addEventListener("DOMContentLoaded", function() {
         h3.innerHTML = 'Welcome to Jeopardy'
 
         startGame.addEventListener('click', () => {
-
             //Add theme music
             const themeMusic = new Audio('./assets/Jeopardy_Intro.mp3');
             themeMusic.play()
-
             startGame.remove()
             modalStart.style.display = 'none'
-
-
-
         })
     }
     startGame()
 
-
-
     for (let cell of cellQ) {
         cell.addEventListener('click', () => {
-
             let value = cell.getAttribute('value')
             let category = cell.getAttribute('category-id')
 
@@ -91,21 +71,15 @@ document.addEventListener("DOMContentLoaded", function() {
             .then(clue => renderClue(clue[0]))
 
             function renderClue(clue) {
-
-
-
                 //Getting question and answer
                 const question = clue.question
                 const clueAnswer = clue.answer.replace('</i>','').replace('<i>','').replace('a ','').replace(/[^\w\s!?]/g,'')
-
-
-                
+               
                 //Add values
                 const h3 = document.querySelector('#questionPopUp h3')
                 const h4 = document.createElement('h4');
                 const h5 = document.createElement('h5');
                 h3.innerText = question
-
                 
                 //Open Modal
                 const modal = document.querySelector("#questionPopUp");
@@ -116,61 +90,41 @@ document.addEventListener("DOMContentLoaded", function() {
                 questionForm.style.display = 'block'
                 const closeBox = document.querySelector('.close')
 
-
-                questionForm.addEventListener('submit', function renderAnswer (e) {
-                    
+                questionForm.addEventListener('submit', function renderAnswer (e) {                   
                     e.preventDefault()                    
-
                     questionForm.style.display = "none";
                     const userAnswer = e.target.answer.value
-
                     //subtracts turn
                     turns--
-
                     closeBox.style.display = 'block';
                     
-                    if (userAnswer.toLowerCase() === clueAnswer.toLowerCase()) {
-                        
+                    if (userAnswer.toLowerCase() === clueAnswer.toLowerCase()) {                      
                         h4.innerText = 'CORRECT!'
                         h4.style.color = 'green'
                         modalContent.appendChild(h4)
                         cell.classList.add('disabled-correct');
-
                         score = score + Number.parseInt(value)
                         scoreSpan.innerText = score
-
                     }
-                    else {
-                        
+                    else {                     
                         h4.innerText = 'INCORRECT!'
                         h4.style.color = 'red'
                         modalContent.appendChild(h4)
-        
                         h5.innerText = `Answer: ${clueAnswer}`
                         modalContent.appendChild(h5);
-                        cell.classList.add('disabled-wrong');
-            
-
+                        cell.classList.add('disabled-wrong');          
                     }
 
-
-
                 if (turns > 0) {                    
-
                     closeBox.addEventListener('click', () =>  {
-
                         questionForm.style.display = 'block';
                         h4.remove()
                         h5.remove()
                         modal.style.display = "none";
                         closeBox.style.display = 'none';
-
-                    }) 
-                    
+                    })                
                 }
                 else {
-
-
                     finishBtn = document.createElement('button')
                     finishBtn.setAttribute('type', 'button')
                     finishBtn.textContent = 'Finish Game!'
@@ -178,7 +132,6 @@ document.addEventListener("DOMContentLoaded", function() {
                     modalContent.appendChild(finishBtn)
 
                     finishBtn.addEventListener('click', () => {
-
                         h3.textContent = `Final Score: ${score}`
                         h4.remove()
                         h5.remove()
@@ -191,42 +144,13 @@ document.addEventListener("DOMContentLoaded", function() {
                         modalContent.appendChild(newGame)
                         
                         newGame.addEventListener('click', () => {
-
                             document.location.reload(true)
-
-                        } )
-
-
-
+                        })
                     })
-                    
-
-
-
                 }
-
-
-
                     questionForm.reset()
-
-
                 }, {once: true} )
-
-
-
-                
-
             }
-
-
         })
-
     }
-
-
-
-
-
 })
-
-
